dbd32d0c6bd8e04ca8e1ea31f01fb513efdd5f72
haseebullah <88248951+Haseebul@users.noreply.github.com>
2024-08-10 23:38:12 +0500

    Create base_notifier.dart

---
+++ lib/core/base/base_notifier.dart
@@ -0,0 +1,30 @@
+import 'package:untitle_isar/path_file.dart';
+import 'package:flutter/material.dart';
+
+class BaseNotifier extends ChangeNotifier with DatabaseManager {
+  bool disposed = false;
+
+  BaseNotifier();
+
+  ViewState _state = ViewState.Idle;
+
+  ViewState get state => _state;
+
+  void setState(ViewState viewState) {
+    _state = viewState;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    disposed = true;
+  }
+
+  @override
+  void notifyListeners() {
+    if (!disposed) {
+      super.notifyListeners();
+    }
+  }
+}
