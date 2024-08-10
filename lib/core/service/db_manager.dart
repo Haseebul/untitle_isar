8de8becdd9dc831957dceb9c5ef0c9af75348976
haseebullah <88248951+Haseebul@users.noreply.github.com>
2024-08-11 01:42:36 +0500

    Update db_manager.dart

--- lib/core/service/db_manager.dart
+++ lib/core/service/db_manager.dart
@@ -1,19 +1,33 @@
-import 'package:isar/isar.dart';
+import 'dart:developer';
 
-// import 'package:untitle_isar/core/model/count/count.dart';
+import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:untitle_isar/core/model/user/user_model.dart';
 
 mixin DBManager {
-  Future<Isar> openDB() async {
+  late Isar isar;
+
+  Future<void> openDB() async {
     final dir = await getApplicationDocumentsDirectory();
-    if (Isar.instanceNames.isNotEmpty) {
-      return await Isar.open(
-        [UserModelSchema],
-        inspector: true,
-        directory: dir.path,
-      );
-    }
-    return Future.value(Isar.getInstance());
+    isar = await Isar.open(
+      [UserModelSchema],
+      inspector: true,
+      directory: dir.path,
+    );
+  }
+
+  Future<void> addUser(UserModel user) async {
+    await isar.writeTxn(() async {
+      await isar.userModels.put(user);
+    });
+  }
+
+  Future<List<UserModel?>> getUsers() async {
+    final users = await isar.userModels.where().findAll();
+    return users;
+  }
+
+  Future<void> deleteUser(int id) async {
+    await isar.userModels.delete(id);
   }
 }
