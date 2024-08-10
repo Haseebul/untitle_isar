9b04e68e89991732d3fbd29fef62d96d78753fd5
haseebullah <88248951+Haseebul@users.noreply.github.com>
2024-08-11 00:28:36 +0500

    Create init_provider.dart

---
+++ lib/core/base/init_provider.dart
@@ -0,0 +1,7 @@
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+List<SingleChildWidget> providers = [
+  //splash or on-boarding
+  // ChangeNotifierProvider(create: (_) => SplashViewModel()),
+];
