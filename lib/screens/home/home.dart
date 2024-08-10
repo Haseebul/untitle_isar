cdf9278805655d4a51bc2f6ec10b23662e3a685c
haseebullah <88248951+Haseebul@users.noreply.github.com>
2024-08-11 01:42:39 +0500

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:untitle_isar/core/base/base_view.dart';
+import 'package:untitle_isar/core/base/view_state.dart';
 import 'package:untitle_isar/screens/home/home_vm.dart';
 
 class Home extends StatelessWidget {
@@ -11,14 +12,52 @@ class Home extends StatelessWidget {
       model: HomeVM(),
       onModelReady: (homeVM) async {
         await homeVM.openDB();
+        await homeVM.getAllUsers();
       },
       builder: (context, homeVM, child) {
         return Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.pink,
           ),
-          body: const Column(
-            children: [],
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Name'),
+                  controller: homeVM.nameTEC,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Age'),
+                  controller: homeVM.ageTEC,
+                ),
+                const SizedBox(height: 20),
+                (homeVM.state == ViewState.Busy)
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: () async {
+                          await homeVM.addUserData();
+                        },
+                        child: const Text('Add User'),
+                      ),
+                const SizedBox(height: 20),
+                (homeVM.loader)
+                    ? const Center(child: CircularProgressIndicator())
+                    : Expanded(
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: homeVM.users.length,
+                          itemBuilder: (context, index) {
+                            final user = homeVM.users[index];
+                            return ListTile(
+                              title: Text(user?.name ?? ''),
+                              subtitle: Text('${user?.age ?? ''}'),
+                            );
+                          },
+                        ),
+                      ),
+              ],
+            ),
           ),
         );
       },
