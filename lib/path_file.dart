73fc0b55d5cb74d2382fca7d071bf73220c41cb5
haseebullah <88248951+Haseebul@users.noreply.github.com>
2024-08-11 00:28:48 +0500

    Create path_file.dart

---
+++ lib/path_file.dart
@@ -0,0 +1,5 @@
+export 'core/base/base_notifier.dart';
+export 'core/base/base_view.dart';
+export 'core/base/init_provider.dart';
+export 'core/base/view_state.dart';
+export 'core/service/db_manager.dart';
