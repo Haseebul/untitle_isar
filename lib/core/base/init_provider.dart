f74b6b3dd65e72fdb2422e61a2ef23baa852607d
haseebullah <88248951+Haseebul@users.noreply.github.com>
2024-08-11 12:40:47 +0500

    Update init_provider.dart

--- lib/core/base/init_provider.dart
+++ lib/core/base/init_provider.dart
@@ -1,7 +1,8 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:untitle_isar/screens/home/home_vm.dart';
 
 List<SingleChildWidget> providers = [
   //splash or on-boarding
-  // ChangeNotifierProvider(create: (_) => SplashViewModel()),
+  ChangeNotifierProvider(create: (_) => HomeVM()),
 ];
